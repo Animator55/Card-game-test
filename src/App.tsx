5716b33bac6e2c874cad9eaaa6907bf739bf580a
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-02-11 17:41:34 -0300

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -38,12 +38,10 @@ const cardsD: cardsList = {
 }
 
 const enemyCards = [
-  "0001.6786", "0002.6876", "0000.68767", "0002.6549", "0003.6546",
-  "0001.3646", "0001.78678", "0003.6876", "0001.9879", "0000.2543",
-  "0002.658746", "0000.67856", "0001.678546", "0003.7897", "0002.425",
+  ["0001.6786", "0002.6876", "0000.68767", "0002.6549", "0003.6546"],
+  ["0001.3646", "0001.78678", "0003.6876", "0001.9879", "0000.2543"],
+  ["0002.658746", "0000.67856", "0001.678546", "0003.7897", "0002.425"]
 ]
-
-let enemyCardsUsed = 0
 
 export default function App() {
   const [round, setRound] = React.useState(0)
@@ -60,6 +58,44 @@ export default function App() {
   const clickCard = (card: string)=>{
     if(!card) return
     setSelected([...selected, card])
+  }
+
+  const generateRandomEnemyCards = ()=>{
+    if(!fight) return [] 
+    let randomCards:string[] = [];
+    let cardsAv = enemyCards[round].filter((el)=>{return el !== ""})
+    if(cardsAv.length === 0) return []
+    let randomAmount = Math.floor(Math.random() * cardsAv.length) + 1
+    for (let i = 0; i < randomAmount; i++) {
+      let index = Math.floor(Math.random() * randomAmount);
+      if(randomCards.includes(cardsAv[index])) {
+        i--
+        continue
+      }
+      randomCards.push(cardsAv[index]);
+      enemyCards[round].splice(enemyCards[round].indexOf(cardsAv[index]), 1, "")
+    }
+    return randomCards;
+  }
+
+  const enemyCardsGenerated = generateRandomEnemyCards()
+
+  const RenderEnemyCards = ()=>{
+    let jsx = []
+
+    for(let i=0; i<5; i++) {
+      if(enemyCards[round][i] === "") continue
+      
+      jsx.push(
+        <div 
+          className="card enemy"
+          key={Math.random()}
+        >
+        </div>
+      )
+    }
+
+    return <div className="hand enemy">{jsx}</div>
   }
 
   const RenderCards = ()=>{
@@ -83,22 +119,10 @@ export default function App() {
     return <div className="hand">{jsx}</div>
   }
 
-  const generateRandomEnemyCards = ()=>{
-    if(!fight) return [] 
-    let randomCards = [];
-    for (let i = 0; i < (enemyCardsUsed >= 5 ? 1 : Math.floor(Math.random() * 5) + 1); i++) {
-        let index = Math.floor(Math.random() * 15);
-        randomCards.push(enemyCards[index]);
-    }
-    enemyCardsUsed += randomCards.length
-    return randomCards;
-  }
-
   const RenderTable = ()=>{
-    const Enemycards = generateRandomEnemyCards()
     return <section className="table">
       <div className="enemy-table">
-        {fight && Enemycards.map(card=>{
+        {fight && enemyCardsGenerated.map(card=>{
           let card_id = card.split(".")[0]
           return <div 
             className="card card-in-table enemy"
@@ -137,9 +161,10 @@ export default function App() {
   }, [fight])
 
   return <main>
+    <RenderEnemyCards/>
     <RenderTable/>
     <RenderCards />
-    <button className="fixed-b" onClick={()=>{enemyCardsUsed=0;setRound(round + 1)}}>Round</button>
+    <button className="fixed-b" onClick={()=>{setRound(round + 1)}}>Round</button>
     <button className="fixed-b-2" onClick={()=>{activateFight(true)}}>Fight</button>
   </main>
 }
