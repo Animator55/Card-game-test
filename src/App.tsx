dc7eabf137be0c65d591d5fe9d32b7598e35ec6d
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-02-29 17:13:42 -0300

    added multiplayer

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,7 @@ type router = {
   [key:string] : IconDefinition
 }
 
-const player = {_id: "a"}
-const enemy = {_id: "b"}
+type dataTransfer = {action: string, cards: string[] | string[][], allCards?: string[][]}
 
 const enemyCards = [
   ["0001.66786", "0002.876", "0000.68757667", "0002.675549", "0003.65756"],
@@ -33,16 +32,37 @@ let justAddedCardEnemy = ""
 let conn: DataConnection | undefined = undefined
 let peer: Peer | undefined = undefined
 
+const generatedTalesCards = (cards: string[][])=>{
+  let newCards = []
+
+  for(let i=0; i < cards.length; i++) {
+    let stack = cards[i].map((card, i)=>{
+      // return card + "."+ Math.floor(Math.random()*100000)
+      return card + "."+ i
+    })
+    newCards.push(stack)
+  }
+  return newCards
+}
+
+let cardsP1 = generatedTalesCards([
+  ["0001", "0002", "0001", "0002", "0003"],
+  ["0001", "0001", "0003", "0001", "0000"],
+  ["0002", "0000", "0001", "0003", "0002"],
+])
+let cardsP2 = generatedTalesCards([
+  ["0002", "0005", "0000", "0003", "0001"],
+  ["0002", "0005", "0003", "0000", "0002"],
+  ["0001", "0005", "0002", "0006", "0000"],
+])
+
 export default function App() {
   const [activateIA, setIA] = React.useState(false)
   const [round, setRound] = React.useState(0)
   const [subRound, setSubRound] = React.useState({index: 0, player: ""})
   const [selected, setSelected] = React.useState<string[]>([])
-  const [cards, setCards] = React.useState([
-    ["0001.6786", "0002.6876", "0000.68767", "0002.6549", "0003.6546"],
-    ["0001.3646", "0001.78678", "0003.6876", "0001.9879", "0000.2543"],
-    ["0002.658746", "0000.67856", "0001.678546", "0003.7897", "0002.425"],
-  ])
+  const [cards, setCards] = React.useState<string[][]>([])
+  const [cardsOpponent, setCardsOpponent] = React.useState<string[][]>([])
 
   const [selectedEnemy, setSelEnemy] = React.useState<string[]>([])
   const [fight, activateFight] = React.useState([false, false])
@@ -59,12 +79,23 @@ export default function App() {
 
   /// FUNCTIONS
 
-  const enemyPicksCard = (newCard: string)=>{
-    enemyCards[round].push(newCard)
+  const enemyPicksCard = (newCard: string, allCards?: string[][])=>{
+    let currentCards = activateIA ? enemyCards[round] : allCards ? allCards[round] : undefined
+    if(currentCards === undefined) return
     justAddedCardEnemy = newCard
+
+    let deleteAmount = 1
+
+    let newCards = cardsOpponent[round].filter((card)=>{
+      if(card === "") deleteAmount = 0
+      else if(deleteAmount === 0 || card !== "") return card
+    })
+
+    if(!activateIA) setCardsOpponent(allCards as string[][])
+    else setCardsOpponent(Object.values({...cardsOpponent, [round]: [...newCards, newCard]}) as Array<string[]>)
     activateFight([false, false])
     setSelEnemy([])
-    setSubRound({index: subRound.index + 1, player: users.player._id })
+    setSubRound({index: subRound.index + 1, player: peer ? peer.id : users.player._id })
   }
 
   const pickCard = (direct: boolean)=>{
@@ -82,9 +113,11 @@ export default function App() {
 
     justAddedCardPlayer = newCard
 
-    setCards(Object.values({...cards, [round]: [...newCards, newCard]}) as Array<string[]>)
+    let cardsResult = Object.values({...cards, [round]: [...newCards, newCard]}) as Array<string[]>
+    setCards(cardsResult)
     activateFight(direct ? [true, true] : [false, false])
     setSelected(direct ? [newCard] : [])
+    if(!direct && !activateIA && conn) conn.send({action: "drawCard", cards: [newCard], allCards: cardsResult})
     setSubRound({index: subRound.index + 1, player: direct ? users.player._id : users.enemy._id })
   }
 
@@ -144,13 +177,20 @@ export default function App() {
 
     peer.on("connection", function (conn: DataConnection | undefined) {
       if(!conn) return
-      console.log(conn.peer + ' is online')
 
       conn.on("data", function (data) { //RECIEVED DATA
         console.log("sended to you " + data)
-        activateFight([fight[0], true])
-        setSelEnemy(data as string[])
-        setSubRound({index: subRound.index, player: users.player._id})
+        if(!peer ||!conn) return
+
+        let Data = data as dataTransfer
+
+        if(Data.action === "drawCard") enemyPicksCard(Data.cards[0] as string, Data.allCards as string[][])
+        else {
+          let playerCurrentFight = Data.action === "respond" 
+          activateFight([playerCurrentFight, true])
+          setSelEnemy(Data.cards as string[])
+          setSubRound({index: subRound.index, player: playerCurrentFight ? conn.peer : peer.id})
+        }
       })
 
       conn.on('close', function () {
@@ -159,31 +199,21 @@ export default function App() {
         conn.close()
         conn = undefined
       })
+      
     });
   }
 
-  /// IA INTERACTIONS
+  const sendSelected = ()=>{
+    if(!peer || !conn) return
 
-  const IArespond = ()=>{
+    conn.send({action: "attack", cards: selected})
     setSubRound({index: subRound.index, player: users.enemy._id})
-    setTimeout(()=>{ // defense bot
-      activateFight([true, true])
-      setSelEnemy(generateRandomEnemyCards(selectedEnemy, enemyCards[round], enemyCards))
-    }, 2000)
   }
 
-  const IAsend = ()=>{
-    setTimeout(()=>{
-      let newCard = pickFromDeck(enemyCards, enemyCards[round])
-      let pickCard = Math.random()>0.6 && enemyCards[round].filter((el)=>{return el!==""}).length < 5 && newCard !== undefined
-      if(pickCard) return enemyPicksCard(newCard)
-      activateFight([false, true])
-      setSelEnemy(generateRandomEnemyCards(selectedEnemy, enemyCards[round], enemyCards))
-      setSubRound({index: subRound.index, player: users.player._id})
-    }, 1000)
-  }
+  const sendResult = (sendRespond: boolean)=>{
+    if(!activateIA && (!peer || !conn)) return
 
-  const IAresult = ()=>{
+    if(sendRespond && !activateIA && conn) conn.send({action: "respond", cards: selected})
     let CalculateFight = calculateFight(selected, selectedEnemy, subRound.player === users.player._id ? users.enemy._id : users.player._id, [users.player._id, users.enemy._id])
     let time = renderFight(CalculateFight, users.player, users.enemy, turnVelocity)
     fadeOutTable(time, turnVelocity, subRound.player === users.player._id)
@@ -192,10 +222,9 @@ export default function App() {
       let newCards = cards[round].map((el)=>{
         return selected.includes(el) ? "": el
       })
-      for (let i = 0; i < enemyCards[round].length; i++) {
-        if(selectedEnemy.includes(enemyCards[round][i])) enemyCards[round].splice(i, 1, "")
-      }
-    
+      let newCardsOpponent = cardsOpponent[round].map((el)=>{
+        return selectedEnemy.includes(el) ? "": el
+      })
       let life1 = 20
       let life2 = 20
       let playerLifeEl = document.querySelector(".player-life") as HTMLElement
@@ -209,6 +238,7 @@ export default function App() {
 
       activateFight([false, false])
       setCards(Object.values({...cards, [round]: newCards}) as Array<string[]>)
+      setCardsOpponent(Object.values({...cardsOpponent, [round]: newCardsOpponent}) as Array<string[]>)
       setSelected([])
       setSelEnemy([])
       setUsers({
@@ -219,14 +249,38 @@ export default function App() {
     }, time*turnVelocity +2000)
   }
 
+  /// IA INTERACTIONS
+
+  const IArespond = ()=>{
+    console.log("defense")
+    setSubRound({index: subRound.index, player: users.enemy._id})
+    setTimeout(()=>{ // defense bot
+      if(cardsOpponent.length === 0) return
+      activateFight([true, true])
+      setSelEnemy(generateRandomEnemyCards(selectedEnemy, cardsOpponent[round], cardsOpponent))
+    }, 2000)
+  }
+
+  const IAsend = ()=>{
+    console.log("attack")
+    setTimeout(()=>{
+      let newCard = pickFromDeck(cardsOpponent, cardsOpponent[round])
+      let pickCard = Math.random()>0.6 && cardsOpponent[round].filter((el)=>{return el!==""}).length < 5 && newCard !== undefined
+      if(pickCard) return enemyPicksCard(newCard)
+      activateFight([false, true])
+      setSelEnemy(generateRandomEnemyCards(selectedEnemy, cardsOpponent[round], cardsOpponent))
+      setSubRound({index: subRound.index, player: users.player._id})
+    }, 1000)
+  }
+
   /// COMPONENTS
 
 
   const RenderEnemyCards = ()=>{
     let jsx = []
-    let currentCards = enemyCards[round]
+    let currentCards = cardsOpponent[round]
 
-    for(let i=0; i<currentCards.length; i++) {
+    if(currentCards !== undefined) for(let i=0; i<currentCards.length; i++) {
       if(currentCards[i] === "" 
       || (selectedEnemy.includes(currentCards[i]) && fight[0] && subRound.player === users.player._id)) continue
        
@@ -329,17 +383,16 @@ export default function App() {
   /// EFFECTS
 
   React.useEffect(()=>{
-    if(users.player._id === "" || users.enemy._id === "") return console.log("user or enemy is not selected")
-
+    if(users.player._id === "" || users.enemy._id === "") return 
     if(subRound.player === "") setSubRound({...subRound, player: users.player._id > users.enemy._id ? users.player._id : users.enemy._id})
   }, [users])
 
   React.useEffect(()=>{
-    if(users.player._id === "" || users.enemy._id === "") return console.log("user or enemy is not selected")
+    if(users.player._id === "" || users.enemy._id === "") return
     if(subRound.player === "") return
 
     if(checkCards(cards[round]) 
-      && checkCards(enemyCards[round]) 
+      && checkCards(activateIA ? enemyCards[round] : cardsOpponent[round]) 
       && selected.length === 0 
       && selectedEnemy.length === 0
     ){
@@ -352,29 +405,34 @@ export default function App() {
   React.useEffect(()=>{
     justAddedCardEnemy = ""
     justAddedCardPlayer = ""
-    if(users.player._id === "" || users.enemy._id === "") return console.log("user or enemy is not selected")
+    if(users.player._id === "" || users.enemy._id === "") return 
 
     if(activateIA) {
       if(fight[0] && !fight[1]) IArespond()
-      else if(fight[0] && fight[1]) IAresult()
+      else if(fight[0] && fight[1]) sendResult(false)
       else if(!fight[0] && !fight[1] && subRound.player !== users.player._id && subRound.player !== "") IAsend()
     }
     else if(peer && conn) {
-      if(fight[0] && !fight[1]) conn.send(selected)
-      else if(fight[0] && fight[1]) conn.send(selected)
+      if(fight[0] && !fight[1]) sendSelected()
+      else if(fight[0] && fight[1] && subRound.player === users.player._id) sendResult(true)
+      else if(fight[0] && fight[1] && subRound.player !== users.player._id) sendResult(false)
     }
   }, [fight]) 
+
+  React.useEffect(()=>{
+    if(cards.length !== 0 && peer === undefined && !activateIA) connection(cards === cardsP1 ? "a" : "b")
+  }, [cards])
 
   let dataTurn = "2"
 
   if(subRound.player === users.player._id) dataTurn = "0"
   else if(subRound.player === users.enemy._id) dataTurn = "1"
 
-  return <main data-turn={subRound.player === users.player._id ? "0" : "1"}>
+  return <main data-turn={dataTurn}>
     <EnemyLife/>
     <RenderEnemyCards/>
     <RenderTable/>
-    <Hand
+    {cards.length !== 0 && <Hand
       confirm={(sel: string[])=>{setSelected(sel); activateFight([true, fight[1]])}}
       users={users}
       subRound={subRound}
@@ -383,20 +441,25 @@ export default function App() {
       selectedReal={selected}
       pickCard={pickCard}
       jstAdCard={justAddedCardPlayer}
-    />
-    <PlayerLife/>
-
+    />}
     <div className="log-with">
       <p style={{color: "white"}}>log-with</p>
-      <button onClick={()=>{connection("a")}}>a</button>
-      <button onClick={()=>{connection("b")}}>b</button>
-      <button onClick={()=>{connection("c")}}>c</button>
+      <button onClick={()=>{
+        if(activateIA) setUsers({player: {...users.player, _id: "a"}, enemy: {...users.enemy, _id: "b"}})
+        setCards(cardsP1)
+        setCardsOpponent(cardsP2)
+      }}>a</button>
+      <button onClick={()=>{
+        if(activateIA) setUsers({player: {...users.player, _id: "b"}, enemy: {...users.enemy, _id: "a"}})
+        setCards(cardsP2)
+        setCardsOpponent(cardsP1)
+      }}>b</button>
     </div>
     <div className="log-with-2">
       <p style={{color: "white"}}>connect to</p>
       <button onClick={()=>{connectToPeer("a")}}>a</button>
       <button onClick={()=>{connectToPeer("b")}}>b</button>
-      <button onClick={()=>{connectToPeer("c")}}>c</button>
     </div>
+    <PlayerLife/>
   </main>
 }
--- src/logic/checkCards.ts
+++ src/logic/checkCards.ts
@@ -1,4 +1,5 @@
 export const checkCards = (deck: string[])=>{ //checks if is necesary to change the round
+  if(!deck) return false
   return !deck.some((card)=>{
     return card !== ""
   })
